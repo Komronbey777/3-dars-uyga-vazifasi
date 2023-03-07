a2bfc48556dab1dbbf88e5c980ad5b4855bf772c
Komronbek <126941442+Komronbey777@users.noreply.github.com>
2023-03-07 08:07:13 +0500

    3-dars

---
+++ main.py
@@ -0,0 +1,34 @@
+while True:
+    inp = int(input("sonni kiriting:"))
+    if inp //3:
+       print("FIZZ")
+    if inp //5:
+        print("BUZZ")
+    if inp //3//5:
+        print("FIZZBUZZ")
+
+    else:
+            print("bu son bo`linmaydi")
+
+log = input ("loginni kiriting")
+parol = int(input("parolni kiriting"))
+email = input("emalni kiriting")
+
+if log=="kamronbek"
+print("salom kamronbek")
+elif parol= "12345678"
+print ("parol to`/g`/ri")
+elif email= "123@gmail.com"
+print ("email to`/g`/ri")
+else:
+    print ("login, parol yoki email adresda xatolik yuz bergan iltimos qayta urinib ko`ring")
+
+a = int(input("FIRST NUMER"))
+b = int(input("SECOND NUMBER"))
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+
+print(c, d, e, f, sep=" => ")
+
